49e5327c504c345e62a3e6230f0498f26b51f988
=juanalumne <=>
2023-05-11 14:08:09 +0200

    hola nbuenas tardes

---
+++ tarea2/bucle-while.py
@@ -0,0 +1,12 @@
+cosas = ['silla',
+         'mesa',
+         'cacerola',
+         'lápiz',
+         'perolingo']
+inventario = {}
+inventario.update({'tenedor':  7})
+
+def funcion (item):
+    numero=int(input('Cuántos'+item+,' hay? ':
+                     inventario.update({item:numero}))                                      
+print(inventario)
